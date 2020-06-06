761aae8364c97d1f8d15bd95d364abdaa1f09e81
亨 刘 <2443558490@qq.com>
2020-06-06 10:19:26 +0800

    服务器功能完成,下一步客户端

---
+++ Client/Client/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace Client
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string address = "127.0.0.1";
+            int port = 8899;
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse(address), port);
+            Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                client.Connect(endPoint);
+                Console.WriteLine("连接成功啦"+client.LocalEndPoint);
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            while (true)
+            {
+
+            }
+
+        }
+    }
+}
